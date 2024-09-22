2fab2ce160a97a1ae01bf2aba33f8c7a7b884bb1
dongnq <quangdong.071094@gmail.com>
2024-09-22 11:55:53 +0700

    add header info user

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,13 @@
-import { Button, Col, Row } from 'antd'
+import { Button, Col, Divider, Row } from 'antd'
 import logo from 'src/assets/icons/logo.png'
+import { Icon } from '../common/icons'
+import useAuth from 'src/store/hooks/authentication'
+import { numberWithCommas } from '../home/Prize/helper'
 
 function Header(props) {
   const { handleClickSignIn, handleClickRegister } = props
   const token = localStorage.getItem('token')
+  const { data } = useAuth()
 
   return (
     <Row
@@ -28,6 +32,19 @@ function Header(props) {
             >
               Register
             </Button>
+          </>
+        )}
+        {token && (
+          <>
+            <div className="flex justify-center items-center">
+              <span className="flex justify-center items-center gap-1 shadow-[inset_0_0_4px_0_#FFFFFF40] p-1 pl-2 pr-2 rounded-lg">
+                <Icon name="dollar" />
+                {numberWithCommas(data?.account?.attributes?.balance)}
+                <Divider type="vertical" style={{  borderColor: '#fff' }}/>
+                <Icon name="topUpWallet" />
+              </span>
+              <span>{data?.account?.attributes?.username}</span>
+            </div>
           </>
         )}
       </Col>
